12ab5c720b3e11a380009d9ca8c3255abefcbceb
Juanfelon14x <juanfelon14x@gmail.com>
2024-09-16 21:36:48 -0500

    Update Bus.java
    Agregué bus a la superclase vehiculo

--- Proyecto/Vehiculos/Bus.java
+++ Proyecto/Vehiculos/Bus.java
@@ -1,5 +1,104 @@
 package Proyecto.Vehiculos;
 
-public class Bus {
+import Proyecto.Vehiculos.Caracteristicas.Antecedente;
+
+public class Bus extends Proyecto.Vehiculo {
+    public int numerodepasajeros;
+    public int numerodepuertas;
+    public int capacidaddelmaletero;
+    public int numerodebolsasdeaire;
+    public int numerodeejes;
+    public int numerodesalidasdeemergencia;
+    public boolean aireacondicionado;
+    public boolean camaradereversa;
+    public boolean abs;
     
+    public Bus(String marca, Antecedente antecedente, int modelo, int cambios, int velocidadmaxima, double cilindraje,
+            int numerodepasajeros, int numerodepuertas, int capacidaddelmaletero, int numerodebolsasdeaire,
+            int numerodeejes, int numerodesalidasdeemergencia, boolean aireacondicionado, boolean camaradereversa,
+            boolean abs) {
+    
+        super(marca, antecedente, modelo, cambios, velocidadmaxima, cilindraje);
+        this.numerodepasajeros = numerodepasajeros;
+        this.numerodepuertas = numerodepuertas;
+        this.capacidaddelmaletero = capacidaddelmaletero;
+        this.numerodebolsasdeaire = numerodebolsasdeaire;
+        this.numerodeejes = numerodeejes;
+        this.numerodesalidasdeemergencia = numerodesalidasdeemergencia;
+        this.aireacondicionado = aireacondicionado;
+        this.camaradereversa = camaradereversa;
+        this.abs = abs;
+    }
+
+    public int getNumerodepasajeros() {
+        return numerodepasajeros;
+    }
+
+    public void setNumerodepasajeros(int numerodepasajeros) {
+        this.numerodepasajeros = numerodepasajeros;
+    }
+
+    public int getNumerodepuertas() {
+        return numerodepuertas;
+    }
+
+    public void setNumerodepuertas(int numerodepuertas) {
+        this.numerodepuertas = numerodepuertas;
+    }
+
+    public int getCapacidaddelmaletero() {
+        return capacidaddelmaletero;
+    }
+
+    public void setCapacidaddelmaletero(int capacidaddelmaletero) {
+        this.capacidaddelmaletero = capacidaddelmaletero;
+    }
+
+    public int getNumerodebolsasdeaire() {
+        return numerodebolsasdeaire;
+    }
+
+    public void setNumerodebolsasdeaire(int numerodebolsasdeaire) {
+        this.numerodebolsasdeaire = numerodebolsasdeaire;
+    }
+
+    public int getNumerodeejes() {
+        return numerodeejes;
+    }
+
+    public void setNumerodeejes(int numerodeejes) {
+        this.numerodeejes = numerodeejes;
+    }
+
+    public int getNumerodesalidasdeemergencia() {
+        return numerodesalidasdeemergencia;
+    }
+
+    public void setNumerodesalidasdeemergencia(int numerodesalidasdeemergencia) {
+        this.numerodesalidasdeemergencia = numerodesalidasdeemergencia;
+    }
+
+    public boolean isAireacondicionado() {
+        return aireacondicionado;
+    }
+
+    public void setAireacondicionado(boolean aireacondicionado) {
+        this.aireacondicionado = aireacondicionado;
+    }
+
+    public boolean isCamaradereversa() {
+        return camaradereversa;
+    }
+
+    public void setCamaradereversa(boolean camaradereversa) {
+        this.camaradereversa = camaradereversa;
+    }
+
+    public boolean isAbs() {
+        return abs;
+    }
+
+    public void setAbs(boolean abs) {
+        this.abs = abs;
+    }
 }
