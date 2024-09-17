39703618653a6db1dff5935b16f01178256d1a05
Juanfelon14x <juanfelon14x@gmail.com>
2024-09-17 18:58:38 -0500

    Update Deportivo.java
    Deportivo ahora esta ligada con la superclase vehiculo

--- Proyecto/Vehiculos/Deportivo.java
+++ Proyecto/Vehiculos/Deportivo.java
@@ -2,13 +2,7 @@ package Proyecto.Vehiculos;
 
 import Proyecto.Vehiculos.Caracteristicas.Antecedente;
 
-public class Deportivo {
-    public String marca;
-    public Antecedente antecedente;
-    public int modelo;
-    public int cambios;
-    public int velocidadmaxima;
-    public double cilindraje;
+public class Deportivo extends Proyecto.Vehiculo {
     public int numerodepasajeros;
     public int numerodepuertas;
     public int numerodebolsasdeaire;
@@ -27,12 +21,7 @@ public class Deportivo {
             int numerodecaballosdefuerza, double tiempoenquealcanzalos100kmh, boolean aireacondicionado,
             boolean camaradereversa, boolean velocidaddecrucero, boolean abs, boolean sensoresdecolision,
             boolean sensordetraficocruzado, boolean asistentedepermanenciaenelcarril) {
-        this.marca = marca;
-        this.antecedente = antecedente;
-        this.modelo = modelo;
-        this.cambios = cambios;
-        this.velocidadmaxima = velocidadmaxima;
-        this.cilindraje = cilindraje;
+        super(marca, antecedente, modelo, cambios, velocidadmaxima, cilindraje);
         this.numerodepasajeros = numerodepasajeros;
         this.numerodepuertas = numerodepuertas;
         this.numerodebolsasdeaire = numerodebolsasdeaire;
@@ -46,54 +35,6 @@ public class Deportivo {
         this.sensordetraficocruzado = sensordetraficocruzado;
         this.asistentedepermanenciaenelcarril = asistentedepermanenciaenelcarril;
     }
-
-    public String getMarca() {
-        return marca;
-    }
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-
-    public Antecedente getAntecedente() {
-        return antecedente;
-    }
-    public void setAntecedente(Antecedente antecedente) {
-        this.antecedente = antecedente;
-    }
-
-
-    public int getModelo() {
-        return modelo;
-    }
-    public void setModelo(int modelo) {
-        this.modelo = modelo;
-    }
-
-
-    public int getCambios() {
-        return cambios;
-    }
-    public void setCambios(int cambios) {
-        this.cambios = cambios;
-    }
-
-
-    public int getVelocidadmaxima() {
-        return velocidadmaxima;
-    }
-    public void setVelocidadmaxima(int velocidadmaxima) {
-        this.velocidadmaxima = velocidadmaxima;
-    }
-
-
-    public double getCilindraje() {
-        return cilindraje;
-    }
-    public void setCilindraje(double cilindraje) {
-        this.cilindraje = cilindraje;
-    }
-
 
     public int getNumerodepasajeros() {
         return numerodepasajeros;
