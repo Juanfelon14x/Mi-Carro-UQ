069954c62b264e32850a5d680fccc3f112aef22f
Juanfelon14x <juanfelon14x@gmail.com>
2024-09-17 19:02:39 -0500

    Update Van.java
    Van ahora esta ligada con la superclase vehiculo

--- Proyecto/Vehiculos/Van.java
+++ Proyecto/Vehiculos/Van.java
@@ -1,5 +1,67 @@
 package Proyecto.Vehiculos;
 
-public class Van {
-    
+import Proyecto.Vehiculos.Caracteristicas.Antecedente;
+
+public class Van extends Proyecto.Vehiculo {
+    public int numerodepasajeros;
+    public int capacidaddelmaletero;
+    public int numerodepuertas;
+    public int numerodebolsasdeaire;
+    public boolean aireacondicionado;
+    public boolean camaradereversa;
+    public boolean abs;
+    public Van(String marca, Antecedente antecedente, int modelo, int cambios, int velocidadmaxima, double cilindraje,
+            int numerodepasajeros, int capacidaddelmaletero, int numerodepuertas, int numerodebolsasdeaire,
+            boolean aireacondicionado, boolean camaradereversa, boolean abs) {
+        super(marca, antecedente, modelo, cambios, velocidadmaxima, cilindraje);
+        this.numerodepasajeros = numerodepasajeros;
+        this.capacidaddelmaletero = capacidaddelmaletero;
+        this.numerodepuertas = numerodepuertas;
+        this.numerodebolsasdeaire = numerodebolsasdeaire;
+        this.aireacondicionado = aireacondicionado;
+        this.camaradereversa = camaradereversa;
+        this.abs = abs;
+    }
+    public int getNumerodepasajeros() {
+        return numerodepasajeros;
+    }
+    public void setNumerodepasajeros(int numerodepasajeros) {
+        this.numerodepasajeros = numerodepasajeros;
+    }
+    public int getCapacidaddelmaletero() {
+        return capacidaddelmaletero;
+    }
+    public void setCapacidaddelmaletero(int capacidaddelmaletero) {
+        this.capacidaddelmaletero = capacidaddelmaletero;
+    }
+    public int getNumerodepuertas() {
+        return numerodepuertas;
+    }
+    public void setNumerodepuertas(int numerodepuertas) {
+        this.numerodepuertas = numerodepuertas;
+    }
+    public int getNumerodebolsasdeaire() {
+        return numerodebolsasdeaire;
+    }
+    public void setNumerodebolsasdeaire(int numerodebolsasdeaire) {
+        this.numerodebolsasdeaire = numerodebolsasdeaire;
+    }
+    public boolean isAireacondicionado() {
+        return aireacondicionado;
+    }
+    public void setAireacondicionado(boolean aireacondicionado) {
+        this.aireacondicionado = aireacondicionado;
+    }
+    public boolean isCamaradereversa() {
+        return camaradereversa;
+    }
+    public void setCamaradereversa(boolean camaradereversa) {
+        this.camaradereversa = camaradereversa;
+    }
+    public boolean isAbs() {
+        return abs;
+    }
+    public void setAbs(boolean abs) {
+        this.abs = abs;
+    }
 }
