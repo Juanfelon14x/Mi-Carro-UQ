84a803a0034e7a13e9f5756e15fe9a1f4cfd1709
Juanfelon14x <juanfelon14x@gmail.com>
2024-11-06 21:24:11 -0500

    Delete Proyecto directory

--- Proyecto/Vehiculos/Caracteristicas/Antecedente.java
+++
@@ -1,6 +0,0 @@
-package Proyecto.Vehiculos.Caracteristicas;
-
-public enum Antecedente {
-    NUEVO,
-    USADO
-}
--- Proyecto/Vehiculos/Caracteristicas/Combustible.java
+++
@@ -1,5 +0,0 @@
-package Proyecto.Vehiculos.Caracteristicas;
-
-public class Combustible {
-    
-}
